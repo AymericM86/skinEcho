c833ac99cb238c698ac3f2f062217f5ea9e0c638
Unknown <madjojo70@live.fr>
2018-04-18 18:12:43 +0200

    Add two buttons

---
+++ Assets/Scripts/Gameplay/GameSequence/SecondProto/TwoButtons.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoButtons : GameSequence
+{
+
+    [SerializeField]
+    VibrationButton m_button1;
+    [SerializeField]
+    VibrationButton m_button2;
+
+    public override bool IsTerminated()
+    {
+        return m_button1.IsTriggered() && m_button2.IsTriggered();
+    }
+
+    public override void DoInUpdate()
+    {
+
+    }
+
+    public override void DoInStart()
+    {
+
+    }
+}
