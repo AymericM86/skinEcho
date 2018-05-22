8d198480ecf5c9c0ae82d32d86127d265e5c297d
Unknown <madjojo70@live.fr>
2018-05-22 16:54:11 +0200

    Monster

--- Assets/Scripts/Gameplay/GameSequence/ThirdProto/MonsterSetupRandom.cs
+++ Assets/Scripts/Gameplay/GameSequence/ThirdProto/MonsterSetupRandom.cs
@@ -20,6 +20,7 @@ public class MonsterSetupRandom : GameSequence {
 
     public override bool IsTerminated()
     {
+        m_monster.gameObject.SetActive(true);
         m_monster.SetupRandom(m_minTimeCalm, m_maxTimeCalm, m_minTimeIrritated, m_maxTimeIrritated);
 
         return true;
